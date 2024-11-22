d124a1577855a33f7b7221ae4d503c6befd8e36e
Summaiyakhan98 <nk4937169@gmail.com>
2024-11-22 10:17:28 +0000

    variables done with note

--- class1/file1.js
+++ class1/file1.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+
+// we have 2 types are veriables when we use
+
+let userId = 1;
+// you can change value of let in the futher:
+const userName = "summai shah";
+// you can't change value of const in the futher:
+
+var userEmail = "summ@ishahgmail.com";
+// i perfer not to use var 
+//because of issue in blockscope and functional scope:
